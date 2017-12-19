c758536045f290235dc4340465ac9ac445bf5097
hela sukkar <32265046+halasukkar97@users.noreply.github.com>
2017-12-19 05:26:36 +0000

    no message

--- Race_Me_assesment/Race_Me/Main.cpp
+++ Race_Me_assesment/Race_Me/Main.cpp
@@ -1,51 +1,16 @@
-#include <windows.h>
-#include <d3d11.h>
-#include <d3dx11.h>
-#include <dxerr.h>
-#include <stdio.h>
-#include"Model.h"
+#include"GameManagment.h"
 #include "camera.h"
 #include "text2D.h"
 #include "Input.h"
-
-#define _XM_NO_INTRINSICS_
-#define XM_NO_ALIGNMENT
-#include <xnamath.h>
+#include "Render.h"
 
 
-int (WINAPIV * __vsnprintf_s)(char *, size_t, const char*, va_list) = _vsnprintf;
 
-
-///////////////////////////
-//	Global Variables
-//////////////////////////////
-
-D3D_DRIVER_TYPE				g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL			g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*				g_pD3DDevice = NULL;
-
-Input*                       input;
-
-
-HINSTANCE	g_hInst = NULL;
-HWND		g_hWnd = NULL;
-
-// Rename for each tutorial
-char		g_TutorialName[100] = "";
-
-///////////////////////////////////////////////////////////////////////////////////////////
-//	Forward declarations
-/////////////////////////////////////////////////////////////////////////////////////////
+GameManagment*		gameManagment;
+Input*				input;
+Render*				render;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-CONSTANT_BUFFER0 cb_values;
-CONSTANT_BUFFER0 cb_values2;
-
-
-void ShutdownD3D();
-
-
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Entry point to the program. Initializes everything and goes into a message processing 
@@ -56,27 +21,27 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
-	if (FAILED(InitialiseWindow(hInstance, nCmdShow)))
+	if (FAILED(gameManagment->InitialiseWindow(hInstance, nCmdShow)))
 	{
 		DXTRACE_MSG("Failed to create Window");
 		return 0;
 	}
 
-	if (FAILED(_Input->Initialise_Input()))
+	if (FAILED(input->Initialise_Input()))
 	{
 		DXTRACE_MSG("Failed to create Input");
 		return 0;
 	}
 
-	if (FAILED(InitialiseD3D()))
+	if (FAILED(gameManagment->InitialiseD3D()))
 	{
 		DXTRACE_MSG("Failed to create Device");
-		ShutdownD3D();
+		gameManagment->ShutdownD3D();
 		return 0;
 	}
 
 	//call initialiserGraphics
-	if (FAILED(InitialiseGraphics())) 
+	if (FAILED(gameManagment->InitialiseGraphics()))
 	{
 		DXTRACE_MSG("Failed to initialise graphics");
 		return 0;
@@ -94,11 +59,11 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		}
 		else
 		{
-			RenderFrame();
+			render->RenderFrame();
 		}
 	}
 
-	ShutdownD3D();
+	gameManagment->ShutdownD3D();
 	return (int)msg.wParam;
 }
 
